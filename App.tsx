ba183333d2175de399a82593b7069541e0a3d8db
almatov1 <almatovalshin@gmail.com>
2024-11-07 10:26:21 +0500

    viro

--- App.tsx
+++ App.tsx
@@ -1,118 +1,67 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { ViroAmbientLight, ViroARScene, ViroARSceneNavigator, ViroBox, ViroDirectionalLight, ViroMaterials, ViroText, ViroTrackingReason, ViroTrackingStateConstants } from '@reactvision/react-viro';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const HelloWorldSceneAR = () => {
+  const [rotation, setRotation] = useState([0, 0, 0]);
+  const [position, setPosition] = useState([0, -0.5, -1]);
+  const [scale, setScale] = useState([0.2, 0.2, 0.2]);
+  const [initialScale, setInitialScale] = useState([0.2, 0.2, 0.2]);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <ViroARScene>
+      {/* Освещение для лучшей видимости текстуры и граней */}
+      <ViroAmbientLight color={"#aaaaaa"} />
+      <ViroDirectionalLight color={"#ffffff"} direction={[0, -1, -0.5]} />
+
+      {/* Куб с текстурой и жестами */}
+      <ViroBox
+        position={position as any}
+        scale={scale as any}
+        rotation={rotation as any}
+        materials={["cubeTexture"]}
+        onDrag={(draggedPosition) => setPosition(draggedPosition)}
+        onPinch={(pinchScaleFactor, pinchState, source) => {
+          if (pinchState === 1) { // Начало жеста "щипка", сохраняем начальный масштаб
+            setInitialScale(scale);
+          }
+
+          if (pinchState === 2) { // Во время жеста "щипка"
+            const newScaleFactor = Math.max(0.05, Math.min(0.5, initialScale[0] * pinchScaleFactor));
+            setScale([newScaleFactor, newScaleFactor, newScaleFactor]);
+          }
+        }}
+
+      // onRotate={(rotateState, rotationFactor, source) => {
+      //   if (rotateState === 3) { // Завершение вращения
+      //     const newRotation = [rotation[0], rotation[1] + rotationFactor, rotation[2]];
+      //     setRotation(newRotation);
+      //   }
+      // }}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    </ViroARScene>
+  );
+};
+
+const App = () => {
+  return (
+    <ViroARSceneNavigator
+      autofocus={true}
+      initialScene={{
+        scene: HelloWorldSceneAR,
+      }}
+      style={styles.f1}
+    />
   );
 }
+export default App;
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+var styles = StyleSheet.create({
+  f1: { flex: 1 }
 });
 
-export default App;
+ViroMaterials.createMaterials({
+  cubeTexture: {
+    diffuseTexture: require('./wood.jpg'), // путь к вашей текстуре
+  },
+});
